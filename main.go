50c403c55ca34b2b4671c798b9045414fe943648
Kirill <kir@mail.ru>
2024-01-12 11:37:27 +0300

    del db

--- main.go
+++ main.go
@@ -2,16 +2,12 @@ package main
 
 import (
 	f_api "TelegramOfficeFood/api"
-	//store "TelegramOfficeFood/db"
 	"net/http"
 	"os"
 )
 
 func main() {
-	//db := store.InitDb()
-	//db.Open(os.Getenv("DATABASE_URL"))
 	api := f_api.ApiInterface{
-		//Store:            db,
 		Port:             os.Getenv("PORT"),
 		Guid:             os.Getenv("GUID"),
 		Token_Telegram:   os.Getenv("TELEGA_TOKEN"),
